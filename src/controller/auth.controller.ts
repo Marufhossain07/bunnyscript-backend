28f51977c2f1708e77cd88a91f4903a3c88440dc
marufhossain07 <marufprogrammer07@gmail.com>
2026-07-16 00:48:56 +0600

    auth controller issue fixed

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -40,7 +40,7 @@ export const login = catchAsync(async (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
